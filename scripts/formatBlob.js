6e4ca0052fa41fe3abac6bfebda4bdc2e1a56db7
Mund <MundVetter@users.noreply.github.com>
2017-10-04 20:32:09 +0200

    added more whitespace

--- scripts/formatBlob.js
+++ scripts/formatBlob.js
@@ -1,9 +1,11 @@
 function formatBlob(blob, width, callback) {
   let reader = new window.FileReader()
   reader.readAsDataURL(blob)
+
   reader.onloadend = function() {
     base64data = reader.result
     let binary = ''
+
     for (let i = 3000; i <3000 + width; i++) {
       const ascii = base64data.charCodeAt(i)
         if(ascii % 2 == 0) {
@@ -12,6 +14,7 @@ function formatBlob(blob, width, callback) {
           binary += '0'
         }
     }
+
     callback(binary)
   }
 }
